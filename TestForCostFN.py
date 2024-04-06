3687dcd3b043a07bd48b6222d055c21a2db7cddb
DillonMUMDGithub <dillon.miller2002@gmail.com>
2024-04-06 14:10:26 -0400

    Just some Updates to the working implementation files ive been working on

--- TestForCostFN.py
+++ TestForCostFN.py
@@ -37,7 +37,7 @@ def plot_curve(Xi,Yi,Thetai,UL,UR):
     t = 0
     r = 14.35#0.038
     L = 28.7#0.354
-    dt = 0.1
+    dt = 0.05
     Xn=Xi
     Yn=Yi
     Thetan = 3.14 * Thetai / 180
@@ -55,15 +55,18 @@ def plot_curve(Xi,Yi,Thetai,UL,UR):
         plt.plot([Xs, Xn], [Ys, Yn], color="blue")
     Thetan = 180 * (Thetan) / 3.14
     return Xn, Yn, Thetan, D
+RPM1 = 10
+RPM2 = 3
 
-actions=[[5,5], [10,10],[5,0],[0,5],[5,10],[10,5]]
+
+actions = [[0,RPM1],[RPM1,0],[RPM1,RPM1],[0,RPM2],[RPM2,0],[RPM2,RPM2],[RPM1,RPM2],[RPM2,RPM1]]
 for action in actions:
-    X1= plot_curve(0,0,45, action[0],action[1]) # (0,0,45) hypothetical start
+    X1= plot_curve(0,0,30, action[0],action[1]) # (0,0,45) hypothetical start
 
     for action in actions:
         X2=plot_curve(X1[0],X1[1],X1[2], action[0],action[1])
 plt.grid()
-ax.set_aspect('equal')
+# ax.set_aspect('equal')
 plt.xlim(0,600)
 plt.ylim(0,200)
 plt.title('How to plot a vector in matplotlib ?',fontsize=10)
