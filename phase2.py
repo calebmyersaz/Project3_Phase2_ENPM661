12d9f151aac83dc84e20c712610e129bae0b392d
HamsaavarthanR <141105847+HamsaavarthanR@users.noreply.github.com>
2024-04-07 12:26:25 -0400

    Updated phase2.py
    I've made changes in backtracking and output format, such that you get a list of Linear and Angular Velocities along with Node states and execution time (t) for each state in seconds.

--- phase2.py
+++ phase2.py
@@ -17,33 +17,39 @@ WheelRadius = 3.3  #33mm
 RobotRadius = 22.0 # 220 mm
 WheelDistance = 28.7 #287mm
 
-# ## Get input for clearance (units) from the obstacle and Step Size of the mobile robot ##
-# clear = int(float(input("Clearance from obstacles and walls (in cm): ")))
-clear = 1
-# radius = int(float(input("Radius of mobile robot: ")))
-
-
-# RPM1 = int(float(input("Input RPM1:")))
-# RPM2 = int(float(input("Input RPM2:")))
-RPM1 = 75
-RPM2 = 35
-WheelRPM = [RPM1,RPM2]
-
-# s = int(float(input("Step size of the mobile robot in range [1,10]: ")))
-# if s<2:
-#     s=2
-# w = int(float(input("Heuristic weightage (Enter 1 for default A* execution): ")))
-w = 2
-clearance = clear + RobotRadius
-# rounded = int(round(clearance/2) + clearance%2)
-rounded = int(clearance)
-border = rounded//2
+while True:
+    RPM1 = int(float(input("Input RPM1:")))
+    RPM2 = int(float(input("Input RPM2:")))
+    # RPM1 = 50
+    # RPM2 = 100
+    if RPM1 <= 0 or RPM2 <= 0:
+        print("Enter a positive non-zero value.")
+    if RPM1 < RPM2:
+        Min = min(RPM1, RPM2, RPM2-RPM1)
+        break
+    elif RPM1 > RPM2:
+        Min = min(RPM1, RPM2, RPM1-RPM2)
+        break
+    else:
+        print("Enter different RPM values for most effective execution.")
 
 actionSet = [[0,RPM1],[RPM1,0],[RPM1,RPM1],[0,RPM2],[RPM2,0],[RPM2,RPM2],[RPM1,RPM2],[RPM2,RPM1]]
+
+## Get input for clearance (units) from the obstacle ##
+clear = int(float(input("Clearance from obstacles and walls (in cm): ")))
+# clear = 1
+
+# w = int(float(input("Heuristic weightage (Enter 1 for default A* execution): ")))
+w = 2
+# 'threshold' within which the goal node must be reached = 3 units
+threshold = 3 
 
 #----------------------------------------------------------------------------------------------------------------------------------------#
 
 ## Define Map ##
+
+clearance = clear + RobotRadius
+rounded = round(clearance)
 
 map = np.ones((200, 600, 3), dtype='uint8')*255
 #Wall Barriers
@@ -98,7 +104,7 @@ for i in range(359,480):
 
 ## Define a 'Node' class to store all the node informations ##
 class Node():
-    def __init__(self, coc=None, cost=None, parent=None, free=False, closed=False):
+    def __init__(self, coc=None, cost=None, parent=None, free=False, closed=False, linVel = 0, angVel = 0):
         # Cost of Coming from 'source' node 
         self.coc = coc
         # Total Cost = Cost of Coming from 'source' node + Cost of Going to 'goal' node 
@@ -109,6 +115,10 @@ class Node():
         self.free = free
         # Boolean variable that denotes (True) if the node is 'closed'
         self.closed = closed
+        # Node as a consequence of applied 'Linear Velocity'
+        self.linVel = linVel
+        # Node as a consequence of applied 'Angular Velocity'
+        self.angVel = angVel
 
 #----------------------------------------------------------------------------------------------------------------------------------------#
 
@@ -130,29 +140,34 @@ for row in range(nodes.shape[0]):
 def backTrack(x,y,l):
     print("Backtracking!!")
     track = []
+    linear = []
+    angular = []
     while True:
         track.append((y,x,l))
+        linear.append(nodes[y][x][l].linVel)
+        angular.append(nodes[y][x][l].angVel)
         if nodes[y][x][l].parent == None:
             track.reverse()
+            linear.reverse()
+            angular.reverse()
             break
         y,x,l = nodes[y][x][l].parent
     print("Path created!")
-    return track
+    return track, linear, angular
 
 #----------------------------------------------------------------------------------------------------------------------------------------#
 
 ## Get 'Source' and 'Goal' node and check if it's reachable ##
 while True:
     print("Node is a point (X,Y) in cartesian plane for X∈[0,600] and Y∈[0,200]")
-    # x1 = int(float(input("X - Coordinate of Source Node: ")))
-    # y1 = int(float(input("Y - Coordinate of Source Node: ")))
-    # x2 = int(float(input("X - Coordinate of Goal Node: ")))
-    # y2 = int(float(input("Y - Coordinate of Goal Node: ")))
-    x1, y1, x2, y2 = (40, 40, 575, 100)
-    print(x1, y1, x2, y2)
+    x1 = int(float(input("X - Coordinate of Source Node: ")))
+    y1 = int(float(input("Y - Coordinate of Source Node: ")))
+    x2 = int(float(input("X - Coordinate of Goal Node: ")))
+    y2 = int(float(input("Y - Coordinate of Goal Node: ")))
+    # x1, y1, x2, y2 = (40, 40, 50, 40)
     print("Orientation of nodes (in degrees) is the direction of mobile robot for theta∈[0,360)")
-    # a1 = int(float(input("Orientation of Source Node (in degrees): ")))
-    a1 = 0
+    a1 = int(float(input("Orientation of Source Node (in degrees): ")))
+    # a1 = 0
     # Convert a1 to the corresponding layer number [0,359] in the 3D array of 'nodes'
     if a1 in (0, 360):
         l1 = a1
@@ -171,7 +186,7 @@ while True:
 
 #----------------------------------------------------------------------------------------------------------------------------------------#
 
-radius = 5 
+# radius = 3
 
 # ## Create a copy of map to store the search state for every 500 iterations ##
 # img = map.copy()
@@ -193,8 +208,16 @@ open_nodes = []
 
 iterations = 0
 start = time.time()
+
+# 'deg' to 'rad' conversion
+pi2 = 2*np.pi
+deg = np.pi/180
+
+# time 't' (in seconds) such that the change in orientation can be atleast 5 degrees
+t = (5*deg) / ((WheelRadius / WheelDistance) * (Min*pi2/60))
+print("time(t) = ", t)
+
 while True:
-    print(x1, y1, l1)
 
     iterations += 1
     if nodes[y1][x1][l1].parent != None:
@@ -215,11 +238,16 @@ while True:
         # Update Total Cost with Cost to Come and Cost to go to the goal is the 'euclidean distance' times the 'Heuristic Weightage'
         nodes[y1][x1][l1].cost = (nodes[y1][x1][l1].coc + (math.sqrt((y2-y1)**2 + (x2-x1)**2))*w)
         
-    # Verify if the current 'open' node is in threshold of 'goal' node (1 units radius)
-    if ((y2-y1)**2 + (x2-x1)**2) <= ((1*radius)**2):
+    # Verify if the current 'open' node is in threshold of 'goal' node (threshold)
+    if ((y2-y1)**2 + (x2-x1)**2) <= ((threshold)**2):
         print("Path Planning Successfull!!!")
+        # Stop the robot at the final node (velocities = 0)
+        nodes[y1][x1][l1].linVel = 0
+        nodes[y1][x1][l1].angVel = 0
         # Call 'Back-Tracking' function
-        path = backTrack(x1,y1,l1)
+        Path, Linear_Vel, Angular_Vel = backTrack(x1,y1,l1)
+        for i in range(len(Linear_Vel)):
+            print(Path[i], Linear_Vel[i], Angular_Vel[i])
         break
 
     # If the current 'node' is not in the threshold region of 'goal' node, 'close' the node and explore neighbouring nodes
@@ -228,35 +256,27 @@ while True:
         nodes[y1][x1][l1].closed = True
         # Perform All Possible Action Sets from actionSet: [[0,RPM1],[RPM1,0],[RPM1,RPM1],[0,RPM2],[RPM2,0],[RPM2,RPM2],[RPM1,RPM2],[RPM2,RPM1]]
         # Get neighbouring nodes to the current 'open' node and add it to the Heap Queue 'open_nodes'
-        
-        # 'deg' to 'rad' conversion
-        pi2 = 2*np.pi
-        deg = np.pi/180
 
-        # time 't' such that the change in orientation can be atleast 2 degrees
-        t = (2*deg) / ((WheelRadius / WheelDistance) * (RPM1*pi2/60))
-
-        # Cost to come of the curren
-        # t open node (y1,x1)
+        # Cost to come of the current open node (y1,x1)
         dist = nodes[y1][x1][l1].coc
 
         # Iterate over 'actions' list 
         for action in actionSet:
-            # Angular velocity of left and right wheels (wl, wr) respectively in rad/sec
+            # Angular velocity of left and right wheels (wl, wr) respectively (in rad/sec)
             wl = (action[0]*pi2) / 60
             wr = (action[1]*pi2) / 60
 
-            # Change in of robot orientation 'theta' in degrees, corresponding to 'action'
+            # Change in of robot orientation 'theta' (in radians), corresponding to 'action'
             theta = (WheelRadius / WheelDistance) * (wl - wr) * t
-            # 'theta' in degrees
-            theta = round(theta / deg)
-            phi = theta+l1
+            # 'theta' (in degrees)
+            theta_deg = round(theta / deg)
+            phi = theta_deg+l1
             if phi < 0:
                 phi = 360 + phi
             elif phi >= 360:
                 phi = 360 - phi
             
-            # Distance traveled along X and Y axis, corresponding to 'action'
+            # Distance traveled along X and Y axis (in cm), corresponding to 'action'
             dy = ((WheelRadius/2) * (wl + wr) * np.sin(phi*deg) * t)
             dx = ((WheelRadius/2) * (wl + wr) * np.cos(phi*deg) * t)
             y = round(y1 + dy)
@@ -291,6 +311,10 @@ while True:
                     # Update 'priority' of new node in 'open_nodes'
                     hq.heappush(open_nodes, (cost, (y, x, l)))
 
+                # Make a note of Linear Velocities(m/s) and Angular Velocities(rad/s) as a consequence of which the new node has arrised
+                nodes[y][x][l].linVel = math.sqrt((dx/t)**2 + (dy/t)**2) / 100
+                nodes[y][x][l].angVel = theta / t
+
         while True:
             # Pop next element from 'open_nodes'
             (priority, node) = hq.heappop(open_nodes)
@@ -301,6 +325,9 @@ while True:
             if priority == (nodes[y][x][l].cost) and nodes[y][x][l].closed == False:
                 break
 
+        # Assign the Linear Velocity and Angular Velocity of current node to parent node
+        nodes[y1][x1][l1].linVel = nodes[y][x][l].linVel
+        nodes[y1][x1][l1].angVel = nodes[y][x][l].angVel
         # Update x1 and y1 for next iteration
         y1 = y
         x1 = x
@@ -312,18 +339,21 @@ while True:
 # cv.circle(img,(xg,yg),radius,(255,0,255),-1)
 # out.write(img)
 print("Number of iterations: ",iterations)
-# #----------------------------------------------------------------------------------------------------------------------------------------#
+
+#----------------------------------------------------------------------------------------------------------------------------------------#
 
 # end = time.time()
 # runntime = end-start
-# print("Path Planning Time: ",runntime)#----------------------------------------------------------------------------------------------------------------------------------------#
+# print("Path Planning Time: ",runntime)
+
+#----------------------------------------------------------------------------------------------------------------------------------------#
 
 # # Iterate over 'optimalPath' and change each pixel in path to 'Red'
 # count = 0
-# for i in range(0,len(path)-1):
+# for i in range(0,len(Path)-1):
 #     count+=1
-#     pt1 = (path[i][1], path[i][0])
-#     pt2 = (path[i+1][1], path[i+1][0])
+#     pt1 = (Path[i][1], Path[i][0])
+#     pt2 = (Path[i+1][1], Path[i+1][0])
 #     cv.line(img,pt1,pt2,(0,0,255),1)
 #     # Write to video file for every 2 iterations
 #     if count/2 == count//2:
@@ -339,4 +369,4 @@ print("Number of iterations: ",iterations)
 
 # out.release()
 
-# #=========================================================================================================================================#
+#=========================================================================================================================================#
